e024c668f9c73626d334c066bf3d964aafe172bf
JosipParlov2012 <josipparlov2012@gmail.com>
2021-03-13 22:33:00 +0100

    Fix perimeter math

--- Polygons/Parallelogram.cs
+++ Polygons/Parallelogram.cs
@@ -9,11 +9,17 @@ namespace Geometry.Polygons
         private readonly Triangle _triangleA;
         private readonly Triangle _triangleB;
 
+        private readonly Vertex _a;
+        private readonly Vertex _b;
+
         public Parallelogram(Point pointA, Point pointB, Point pointC, Point pointD)
         {
             _triangleA = new Triangle(pointA, pointB, pointC);
             _triangleB = new Triangle(pointA, pointD, pointC);
 
+            _a = _triangleA.VertexAB;
+            _b = _triangleB.VertexAB;
+            
             if (Util.FloatEquals(_triangleA.Perimeter(), _triangleB.Perimeter())) return;
             if (_triangleA.IsEquilateral() && _triangleB.IsEquilateral()) return;
             throw new ArgumentException("not quadrilateral");
@@ -26,7 +32,7 @@ namespace Geometry.Polygons
 
         public override float Perimeter()
         {
-            return _triangleA.Perimeter() + _triangleB.Perimeter();
+            return 2 * (_a.Length() + _b.Length());
         }
         
     }
